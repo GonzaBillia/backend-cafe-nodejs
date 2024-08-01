0b7729fb5cb2e77c92a0de3457ac4e104de0da7c
Gonzalo Billia <gonzalobillia@gmail.com>
2024-07-31 22:45:14 -0300

    creo gets, update y delete de producto

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,5 +1,5 @@
-import { ERROR_CREATE_PRODUCT, ERROR_QUERY } from "../constants/errors.constant.js";
-import { SUCCESS_GET_ALL, SUCCESS_PRODUCT_CREATED } from "../constants/messages.constant.js";
+import { ERROR_CREATE_PRODUCT, ERROR_DELETE_PRODUCT, ERROR_PRODUCT_NOT_FOUND, ERROR_QUERY } from "../constants/errors.constant.js";
+import { SUCCESS_GET_ALL, SUCCESS_PRODUCT_CREATED, SUCCESS_PRODUCT_DELETED, SUCCESS_PRODUCT_UPDATED } from "../constants/messages.constant.js";
 import connection from "../db/connection.js";
 
 export const getProducts = (req, res) => {
@@ -13,7 +13,30 @@ export const getProducts = (req, res) => {
     })
 }
 
-export const getProduct = async (req, res) => {}
+export const getProduct = (req, res) => {
+    let id = req.params.id
+    let query = 'SELECT p.id, p.title, p.description, p.price/100, p.code, p.stock, p.status, p.thumbnail, c.name AS category FROM product p INNER JOIN category c ON p.category_id = c.id WHERE p.id = ?'
+    connection.query(query, [id], (err, result) => {
+        if (!err) {
+            return res.status(200).send({message: SUCCESS_GET_ALL, payload: result})
+        } else {
+            return res.status(500).send({message: ERROR_QUERY, error: err})
+        }
+    })
+}
+
+export const getByCategory = (req, res) => {
+
+    let id = req.params.id
+    let query = 'SELECT id, title FROM product WHERE category_id = ? AND status = "true"'
+    connection.query(query, [id], (err, result) => {
+        if(!err){
+            return res.status(200).send({message: SUCCESS_GET_ALL, payload: result})
+        } else {
+            return res.status(500).send({message: ERROR_QUERY, error: err})
+        }
+    })
+}
 
 export const postProduct = (req, res) => {
     let product = req.body
@@ -28,6 +51,35 @@ export const postProduct = (req, res) => {
     })
 }
 
-export const updateProduct = async (req, res) => {}
+export const updateProduct = async (req, res) => {
+    let id = req.params.id
+    let product = req.body
+    let query = 'UPDATE product SET title = ?, description = ?, price = ?, category_id = ?, code = ?, stock = ?, thumbnail = ? WHERE id = ?'
+    connection.query(query, [product.title, product.description, product.price, product.category_id, product.code, product.stock, product.thumbnail, id], (err, result) => {
+        if (!err) {
+            if(result.affectedRows == 0){
+                return res.status(404).send({message: ERROR_PRODUCT_NOT_FOUND, error: err})
+            } else {
+                return res.status(200).send({message: SUCCESS_PRODUCT_UPDATED, payload: result})
+            }
+        } else {
+            return res.status(500).send({message: ERROR_DELETE_PRODUCT, error: err})
+        }
+    })
+}
 
-export const deleteProduct = async (req, res) => {}
+export const deleteProduct = async (req, res) => {
+    let id = req.params.id
+    let query = 'DELETE FROM product WHERE id = ?'
+    connection.query(query, [id], (err, result) => {
+        if (!err) {
+            if(result.affectedRows == 0){
+                return res.status(404).send({message: ERROR_PRODUCT_NOT_FOUND, error: err})
+            } else {
+                return res.status(200).send({message: SUCCESS_PRODUCT_DELETED, payload: result})
+            }
+        } else {
+            return res.status(500).send({message: ERROR_DELETE_PRODUCT, error: err})
+        }
+    })
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,12 +1,20 @@
 import Router from "express"
 import { authenticate } from "../middlewares/authentication.js"
 import { checkRole } from "../middlewares/checkRole.js"
-import { getProducts, getProduct, postProduct, updateProduct, deleteProduct } from "../controllers/product.controller.js"
+import { getProducts, getProduct, postProduct, updateProduct, deleteProduct, getByCategory } from "../controllers/product.controller.js"
 
 const router = Router()
 
 router.get("/get", authenticate, getProducts)
 
+router.get("/getById/:id", authenticate, getProduct)
+
+router.get("/getByCategory/:id", authenticate, getByCategory)
+
 router.post("/add", authenticate, checkRole, postProduct)
+
+router.put("/update/:id", authenticate, checkRole, updateProduct)
+
+router.delete("/delete/:id", authenticate, checkRole, deleteProduct)
 
 export default router
